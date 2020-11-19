368f8c6d26b7c9d7ace13a9d357e34533aa9b5ed
clairebogdan <bogdanclaire@gmail.com>
2020-11-19 16:12:33 -0500

    third commit

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+    serverId = 11110;
+    serverStatus = 'offline';
+
+    getServerStatus() {
+        if (Math.random() < 0.5) {
+            this.serverStatus = 'offline';
+            console.log(this.serverStatus);
+            return this.serverStatus;
+        }
+        else if (Math.random() >= 0.5) { 
+            this.serverStatus = 'online';
+            console.log(this.serverStatus);
+            return this.serverStatus;
+        }
+        else {
+            return this.serverStatus;
+        }
+    }
+}
